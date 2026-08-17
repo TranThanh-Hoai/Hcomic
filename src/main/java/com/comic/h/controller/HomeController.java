b25c9b1de9d9ce1af995a316117cee86049a794e
TranHoai <tthoai654@gmail.com>
2026-08-17 12:15:01 +0700

    feat: add home controller

---
+++ src/main/java/com/comic/h/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.comic.h.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Controller
+@Hidden
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
